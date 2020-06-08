3df5dbaf9e15ceb5177bd6612ec18c4b31774b6a
Jon <jon.showard@gmail.com>
2020-06-08 20:23:22 -0230

    progress

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-  return <>
-    <Text style={{ fontSize: 48 }}>Signup Screen</Text>
-    <Button
-      title="Go To Signin" 
-      onPress={() => navigation.navigate('Signin')} 
-    />
-    <Button
-      title="Go to mainflow" 
-      onPress={() => navigation.navigate('mainFlow')} 
-    />
-  </>
+  return <View style={styles.container}>
+    <Spacer>
+      <Text h3>Sign Up for Tracker</Text>
+    </Spacer>
+    <Input label="Email" />
+    <Spacer />
+    <Input label="Password" />
+    <Spacer>
+      <Button title="Sign Up" />
+    </Spacer>
+  </View>
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SignupScreen;
